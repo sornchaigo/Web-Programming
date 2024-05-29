35686b36b4b9b66ba601153bab49c7c87bab4c10
sornchaigo <sornchai.ud@go.buu.ac.th>
2024-05-29 14:46:42 +0700

    Lab11: Add Subject Form

--- Lab11/main.py
+++ Lab11/main.py
@@ -1,7 +1,10 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Query, Body, Form
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+
+from typing import Annotated
+from pydantic import BaseModel, Field
 
 app = FastAPI()
 
@@ -44,6 +47,59 @@ async def welcome(request: Request, username: str = "Anonymous"):
 async def welcome(request: Request, username: str = "Anonymous"):
     return templates.TemplateResponse(
         request=request, name="welcome.html", context={"username": username}
+    )
+
+
+# listSubject = ["Digital Skill", "Web Programming", "Programming I", "Statistic"]
+listSubject = [
+    {"id": 1, "name": "Digital Skill"},
+    {"id": 2, "name": "Web Programming"},
+    {"id": 3, "name": "Programming I"},
+    {"id": 4, "name": "Statistic"},
+]
+
+
+async def searchSubject(listSubject: list, keyword: str):
+    return [word for word in listSubject if keyword in word["name"]]
+    # response = []
+    # for word in listSubject:
+    #     if keyword in word:
+    #         response.append(word)
+    # return response
+
+
+@app.get("/subject", response_class=HTMLResponse)
+async def subject(request: Request, keyword: str | None = None):
+    matchSubject = listSubject
+    if keyword:
+        matchSubject = await searchSubject(listSubject, keyword)
+
+    return templates.TemplateResponse(
+        request=request,
+        name="subject.html",
+        context={"subjectList": matchSubject},
+    )
+
+
+class Subject(BaseModel):
+    id: int = Form()
+    name: str = Form()
+
+
+@app.post("/subject")
+async def create_subject(
+    request: Request,
+    subject_id: Annotated[str, Form()],
+    subject_name: Annotated[str, Form()],
+):
+    sub = {"id": subject_id, "name": subject_name}
+    if not sub in listSubject:
+        listSubject.append(sub)
+
+    return templates.TemplateResponse(
+        request=request,
+        name="subject.html",
+        context={"subjectList": listSubject},
     )
 
 
